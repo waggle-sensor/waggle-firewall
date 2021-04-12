a4928583be8b21e53fda61cdd33b97f9e65f83e7
Joe Swantek <joseph.swantek@northwestern.edu>
2021-04-12 17:59:28 -0500

    SAGE-583: read rules from required parameter; remove interface configs
    Remove the hard-coded interface config lookup and instead read the
    rules to apply (or remove) from the required '-r' parameter.
    This enables the rules to be changed on a running system and the flexibility
    to have different rules per interface.

--- release.sh
+++ release.sh
@@ -21,6 +21,9 @@ cp -p deb/install/postinst ${BASEDIR}/DEBIAN/
 cp -p deb/install/prerm ${BASEDIR}/DEBIAN/
 
 cp -pr ROOTFS/* ${BASEDIR}/
+# touch empty config file
+mkdir -p ${BASEDIR}/etc/waggle/firewall/
+touch ${BASEDIR}/etc/waggle/firewall/rules
 
 dpkg-deb --root-owner-group --build ${BASEDIR} "${NAME}_${VERSION_SHORT}_${ARCH}.deb"
 mv *.deb /output/
